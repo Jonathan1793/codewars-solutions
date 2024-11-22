b4b31b92f448fecb0ab3860ed307955fb16da796
Jonathan Ruiz <jonathan.ruiz.1793@gmail.com>
2024-11-21 23:36:27 -0600

    modifications to secondsToHuman.js

--- 5kyu/secondsToHumanRead.js
+++ 5kyu/secondsToHumanRead.js
@@ -4,9 +4,13 @@ function humanReadable(number) {
   let seconds = 0;
 
   hours = Math.floor(number / 3600);
+  console.log(hours);
   seconds = number % 3600;
+  console.log(seconds);
   minutes = Math.floor(seconds / 60);
+  console.log(minutes);
   seconds = seconds % 60;
+  console.log(seconds);
 
   if (hours < 10) hours = "0" + hours;
   if (minutes < 10) minutes = "0" + minutes;
@@ -14,3 +18,5 @@ function humanReadable(number) {
 
   return `${hours}:${minutes}:${seconds}`;
 }
+
+console.log(humanReadable(59860));
